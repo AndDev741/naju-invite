4dc945400de3dc63027fd88bfaf89f5c8b0a15e1
AndDev741 <anddesenvolved@gmail.com>
2025-02-09 16:46:52 -0300

    feat: Location page implementated

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router'
 import GiftSugestion from './pages/giftSugestion'
 import Confirmation from './pages/confirmation'
+import Location from './pages/location'
 
 function App() {
   return (
     <div className='bg-[#041C34]'>
       <Routes>
         <Route path='/' element={<Confirmation/>} />
-        <Route path='/location' element={<Location/>} />
+        <Route path='/localizacao' element={<Location/>} />
         <Route path='/sugestao' element={<GiftSugestion/>} /> 
       </Routes>
     </div>
--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -1,7 +1,28 @@
+import Header from "../component/header";
+import { Map, Marker, ZoomControl } from "pigeon-maps";
+
 export default function Location(){
     return(
-        <div>
-        
-        </div>
+        <>
+            <Header/>
+            <main className="flex flex-col items-center mt-8 min-h-[100vh]">
+                <h2 className="text-white text-2xl mb-3">
+                    Localização
+                </h2>
+                <div className="flex items-center justify-center w-[90vw] h-[300px] md:w-[700px] md:h-[400px] mb-5 md:mb-0">
+                    <Map  defaultCenter={[-16.8103, -49.2973]} defaultZoom={15} metaWheelZoom={true}>
+                        <Marker width={50} anchor={[-16.8103, -49.2973]} />
+                        <ZoomControl />
+                    </Map>
+                </div>
+                <p className="text-center text-white text-lg">
+                Alameda Dona Gercina Borges, Qd K-41, chácara 6 - Bairro Independência, Aparecida de Goiânia - GO, 74967-470
+                </p>
+                <a className="underline text-xl text-[#DAA520]" 
+                href="https://www.google.com/maps/place/Espa%C3%A7o+De+Eventos+Princesa/@-16.8106132,-49.2982534,17.29z/data=!4m6!3m5!1s0x935ef9ce347f7d09:0x71bcfaa31423bd21!8m2!3d-16.8103732!4d-49.2969748!16s%2Fg%2F11l6x0vzk6?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D"
+                target="_blank">
+                    Veja no google maps</a>
+            </main>
+        </>
     )
 }
